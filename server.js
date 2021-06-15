8005b8c302b8a1c61cc0de3caa2940473d36a0c9
Celina Louissaint <celinalouissaint@gmail.com>
2021-06-14 17:46:16 -0700

    file structure

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+// add mongoose at start
+const mongoose = require('mongoose');
+
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.use(require('./routes'));
+
+
+
+// mongoose.connect tells mongoose which database to connect to 
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/her', {
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+// Use this to log mongo queries being executed!
+mongoose.set('debug', true);
+
+
+app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
